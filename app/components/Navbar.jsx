0972d895436cd75d35def028b94e34954c18314f
Don Raul <106396891+Villanueva0909@users.noreply.github.com>
2025-02-14 16:22:32 -0500

    .

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link href={'/'}>Navbar Link </Link>
+        </nav>
+    )
+}
+
+export default Navbar;
