60c0bff28eee6fc78391ce8dcc3f7f772dc76418
Don Raul <106396891+Villanueva0909@users.noreply.github.com>
2025-02-15 21:55:32 -0500

    Information not provided by developer

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,10 +2,11 @@ import Link from "next/link"
 
 const Navbar = () => {
     return (
-        <nav>
-            <Link href={'/'}>Navbar Link </Link>
+        <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
+            <Link className="text-white font-bold" href={'/'}>RR Coding </Link>
+            <Link className="bg-white p-2" href={'/addtopic'}>Add topic</Link>
         </nav>
     )
 }
 
-export default Navbar;
+export default Navbar; 
---
+++ app/components/TopicsList.jsx
@@ -0,0 +1,14 @@
+const TopicsList = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <h2>Topic Title</h2>
+                    <div></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default TopicsList
