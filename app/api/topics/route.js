1ba88af45334c6e68126ab14438598b4e422cd5b
Don Raul <106396891+Villanueva0909@users.noreply.github.com>
2025-02-18 12:58:03 -0500

    Updating backend to mongodb

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -1,9 +1,10 @@
 import connectMongoDB from "@/libs/mongodb"
 import { NextResponse } from "next/server"
+import Topic from "@/models/topic"
 
 export async function POST(request) {
     const { title, description } = await request.json()
-    await connectMongoDB
+    await connectMongoDB()
     await Topic.create({ title, description })
     return NextResponse.json({ message: "Topic created" }, { status: 201 })
 }
