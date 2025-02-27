de8d064368ea14076584fcc4f3d950d64a2c9da5
Don Raul <106396891+Villanueva0909@users.noreply.github.com>
2025-02-27 15:25:26 -0500

    Information not provided by developer.

--- app/components/TopicsList.jsx
+++ app/components/TopicsList.jsx
@@ -1,22 +1,44 @@
 import RemoveBtn from "./RemoveBtn"
 import { HiPencilSquare } from "react-icons/hi2";
 import Link from "next/link";
+import { cache } from "react";
 
-const TopicsList = () => {
+const getTopics = async () => {
+    try {
+        const res = await fetch('http://localhost:3001/api/topics', {
+            cache: "no-store"
+        })
+
+        if (!res.ok) {
+            throw new Error("Failed to fetch topics")
+        }
+
+        return res.json()
+    } catch (error) {
+        console.log("Error loading topics", error)
+    }
+}
+
+const TopicsList = async () => {
+
+    const { topics } = await getTopics()
+
     return (
         <>
-            <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-                <div>
-                    <h2 className="font-bold text-2xl" >Topic Title</h2>
-                    <div>Topic Description</div>
+            {topics.map(t => (
+                <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+                    <div>
+                        <h2 className="font-bold text-2xl" >{t.title}</h2>
+                        <div>{t.description}</div>
+                    </div>
+                    <div className="flex gap-2">
+                        <RemoveBtn />
+                        <Link href={'/editTopic/${t._id}'}>
+                            <HiPencilSquare size={24} />
+                        </Link>
+                    </div>
                 </div>
-                <div className="flex gap-2">
-                    <RemoveBtn />
-                    <Link href={'/editTopic/123'}>
-                        <HiPencilSquare size={24} />
-                    </Link>
-                </div>
-            </div>
+            ))}
         </>
     )
 }
