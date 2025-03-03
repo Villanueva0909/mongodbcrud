f04e0e83e539a1f7624b3c8dfcab4783283720f8
Don Raul <106396891+Villanueva0909@users.noreply.github.com>
2025-03-02 19:12:30 -0500

    Updating MongoDB

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
+
 const addTopic = () => {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("")
+    const handleSubmit = () => {
+        
+    }
+
     return (
         <>
             <form className="flex flex-col gap-3">
-                <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
+                <input
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
                 <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Description" />
                 <button className="bg-red-600 font-bold text-white py-3 px-6 w-fit rounded-lg">Add Topic</button>
             </form>
