429f6b26a2589a269751c2a3b9ab7b563ef6e717
Don Raul <106396891+Villanueva0909@users.noreply.github.com>
2025-03-11 14:47:24 -0400

    Updating removebuttons

--- app/components/RemoveBtn.jsx
+++ app/components/RemoveBtn.jsx
@@ -6,12 +6,14 @@ const RemoveBtn = (id) => {
         const confirmed = confirm("Are you sure?")
 
         if(confirmed) {
-            await fetch('http')
+            await fetch(`http://localhost:3000/api/topics?id=${id}` || `http://localhost:3001/api/topics?id=${id}`, {
+                method: "DELETE"
+            })
         }
     }
 
     return (
-        <button className="text-red-400">
+        <button onClick={removeTopic} className="text-red-400">
             <FaRegTrashAlt size={24} />
         </button>
     )
