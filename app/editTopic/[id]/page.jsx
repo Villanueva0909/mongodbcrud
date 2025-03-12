feb0b9fda541654c85ba605b093501777ee8216d
Don Raul <106396891+Villanueva0909@users.noreply.github.com>
2025-03-11 20:31:43 -0400

    Updating DB

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,16 +1,26 @@
 import EditTopicForm from "@/app/components/EditTopicForm"
 
-const getTopicById = async() => {
+const getTopicById = async () => {
     try {
-        const res = await fetch('http://localhost:3000/api/topics/')
+        const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+            cache: "no-store"
+        })
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch topic')
+        }
+
+        return res.json()
     } catch (error) {
-        
+        console.log(error)
+
     }
 
 }
 
-const editTopic = ({ params }) => {
-    const { id } = params
+const editTopic = async ({ params }) => {
+    const { id } = params;
+    await getTopicById(id)
     return (
         <EditTopicForm />
     )
