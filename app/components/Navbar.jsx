68734336235c092f2a33aa28500f1fddd2244d87
Don Raul <106396891+Villanueva0909@users.noreply.github.com>
2025-03-18 22:55:31 -0400

    .

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,6 +7,11 @@ const Navbar = () => {
                 <Link className="text-white font-bold" href={'/'}>RR Coding </Link>
                 <Link className="bg-white p-2 rounded-lg" href={'/addTopic'}>Add topic</Link>
             </nav>
+
+            <nav className="flex justify-between items-center bg-slate-800 px-8 py-3 w-lg rounded-lg">
+                <Link className="text-white font-bold" href={'/'}>RR Coding </Link>
+                <Link className="bg-white p-2 rounded-lg" href={'/addTopic'}>Add topic</Link>
+            </nav>
         </>
     )
 }
